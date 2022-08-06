daae698b6d86864065c76eac66254fc432b6f6f9
jiyeon min <ichbinmin2@gmail.com>
2022-08-06 22:13:43 +0900

    ✨ 게시글 목록 부분 컴포넌트 구현하기

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -4,6 +4,7 @@ import Footer from 'components/Common/Footer'
 import GlobalStyle from 'components/Common/GlobalStyle'
 import Introduction from 'components/Main/introduction'
 import CategoryList from 'components/Main/CategoryList'
+import PostList from 'components/Main/PostList'
 
 const CATEGORY_LIST = {
   All: 5,
@@ -23,6 +24,7 @@ const IndexPage: FunctionComponent = function () {
       <GlobalStyle />
       <Introduction />
       <CategoryList selectedCategory="Web" categoryList={CATEGORY_LIST} />
+      <PostList />
       <Footer />
     </Container>
   )
