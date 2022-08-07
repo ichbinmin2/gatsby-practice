a506b30dfcc0781d5aa5ffaf8c68e6e9786a14ac
JiyeonMin <ichbinmin2@gmail.com>
2022-08-07 21:04:01 +0900

    ✨ 메인 페이지에서 Props로 받아 포스트 데이터 출력하기

--- src/components/Main/PostList.tsx
+++ src/components/Main/PostList.tsx
@@ -1,23 +1,11 @@
 import React, { FunctionComponent } from 'react'
 import styled from '@emotion/styled'
 import PostItem from './PostItem'
+import { PostListItemType } from '../../types/PostItem.types'
 
-const PROFILE_IMAGE_LINK = `../../images/profileImg.jpeg`
-
-const POST_ITEM_DATA = {
-  title: 'Post Item Title',
-  date: '2020.01.29.',
-  categories: ['Web', 'Frontend', 'Testing'],
-  summary:
-    'Lorem ipsum dolor sit amet consectetur adipisicing elit. Provident repellat doloremque fugit quis rem temporibus! Maxime molestias, suntrem debitis odit harum impedit. Modi cupiditate harum dignissimos eos in corrupti!',
-  thumbnail: `${PROFILE_IMAGE_LINK}`,
-  link: 'https://www.google.co.kr',
+type PostListProps = {
+  posts: PostListItemType[]
 }
-
-// >> 트러블 슈팅!
-// 또 이미지가 보이지 않는다!
-// images 폴더에 저장한 이미지 경로를 불러왔음에도 404가 뜬다.
-// 이 부분에 대한 이슈를 해결해야만 할 것 같다.
 
 const PostListWrapper = styled.div`
   display: grid;
@@ -34,13 +22,12 @@ const PostListWrapper = styled.div`
   }
 `
 
-const PostList: FunctionComponent = function () {
+const PostList: FunctionComponent<PostListProps> = function ({ posts }) {
   return (
     <PostListWrapper>
-      <PostItem {...POST_ITEM_DATA} />
-      <PostItem {...POST_ITEM_DATA} />
-      <PostItem {...POST_ITEM_DATA} />
-      <PostItem {...POST_ITEM_DATA} />
+      {posts.map(({ node: { id, frontmatter } }: PostType) => (
+        <PostItem {...frontmatter} link="https://www.google.co.kr/" key={id} />
+      ))}
     </PostListWrapper>
   )
 }
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -5,6 +5,16 @@ import GlobalStyle from 'components/Common/GlobalStyle'
 import Introduction from 'components/Main/introduction'
 import CategoryList from 'components/Main/CategoryList'
 import PostList from 'components/Main/PostList'
+import { graphql } from 'gatsby'
+import { PostListItemType } from 'types/PostItem.types'
+
+type IndexPageProps = {
+  data: {
+    allMarkdownRemark: {
+      edges: PostListItemType[]
+    }
+  }
+}
 
 const CATEGORY_LIST = {
   All: 5,
@@ -18,13 +28,17 @@ const Container = styled.div`
   height: 100%;
 `
 
-const IndexPage: FunctionComponent = function () {
+const IndexPage: FunctionComponent<IndexPageProps> = function ({
+  data: {
+    allMarkdownRemark: { edges },
+  },
+}) {
   return (
     <Container>
       <GlobalStyle />
       <Introduction />
       <CategoryList selectedCategory="Web" categoryList={CATEGORY_LIST} />
-      <PostList />
+      <PostList posts={edges} />
       <Footer />
     </Container>
   )
@@ -32,3 +46,26 @@ const IndexPage: FunctionComponent = function () {
 }
 
 export default IndexPage
+
+export const getPostList = graphql`
+  query getPostList {
+    allMarkdownRemark(
+      sort: { order: DESC, fields: [frontmatter___date, frontmatter___title] }
+    ) {
+      edges {
+        node {
+          id
+          frontmatter {
+            title
+            summary
+            date(formatString: "YYYY.MM.DD.")
+            categories
+            thumbnail {
+              publicURL
+            }
+          }
+        }
+      }
+    }
+  }
+`
